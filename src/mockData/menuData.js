9f85404483d7da798faadc5300aace2e11f4c5d3
cloud.n <vannt@mor.com.vn>
2026-06-18 16:21:37 +0700

    fix bug

--- src/mockData/menuData.js
+++ src/mockData/menuData.js
@@ -96,6 +96,16 @@ export const MENU_DATA = [
     status: 1,
   },
   {
+    id: "055",
+    name: "Tích Hợp TXNG Quốc Gia",
+    url: "/tich_hop_txng_quoc_gia",
+    isCompany: 0,
+    sortOrder: 4,
+    parentID: "001",
+    uniqueCode: "NationalPortalIntegrations",
+    status: 1,
+  },
+  {
     id: "010",
     name: "Quản lí thông tin DN/CN",
     url: "/quan_ly_thong_tin_dn",
@@ -225,16 +235,6 @@ export const MENU_DATA = [
     sortOrder: 9,
     parentID: "005",
     uniqueCode: "ExportProducts",
-    status: 1,
-  },
-  {
-    id: "055",
-    name: "Tích Hợp TXNG Quốc Gia",
-    url: "/tich_hop_txng_quoc_gia",
-    isCompany: 0,
-    sortOrder: 10,
-    parentID: "005",
-    uniqueCode: "NationalPortalIntegrations",
     status: 1,
   },
   {
